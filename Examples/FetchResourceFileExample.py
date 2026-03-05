5257fa34213db3c7350a52686c3abd6a9b8ec3c9
Matt Prilliman <54449384+mjprilliman@users.noreply.github.com>
2026-03-05 11:41:29 -0600

    Update default tmy endpoint from NSRDB fetcher (#202)
    * Update default tmy endpoint from NSRDB fetcher
    
    * Update function descriptions, examples
    
    * Add back geocoder
    
    * Update tests
    
    * Update NSRDB endpoint descriptions
    
    ---------
    
    Co-authored-by: Paul Gilman <cpaulgilman@gmail.com>

--- Examples/FetchResourceFileExample.py
+++ Examples/FetchResourceFileExample.py
@@ -23,6 +23,7 @@ import PySAM.Pvwattsv8 as pv
 
 import geocoder
 
+
 # replace with key and email address from https://developer.nrel.gov/signup/
 sam_api_key = os.environ.get('NREL_API_KEY')
 sam_email = os.environ.get('NREL_API_EMAIL')
@@ -39,7 +40,7 @@ if geocode:
     lat = g.latlng[0]
 else:
     lon = -105.22
-    lat = 39.75
+    lat = 45
 
 # --- Wind Example ---
 
@@ -83,6 +84,7 @@ else:
 nsrdbfetcher = tools.FetchResourceFiles(
                 tech='solar',
                 nrel_api_key=sam_api_key,
+                resource_type = 'nsrdb-GOES-tmy-v4-0-0',
                 nrel_api_email=sam_email)
 
 # --- List of (lon, lat) tuples or Shapely points ---
