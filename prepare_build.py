eb3b0da2d032dcd10d39ef130567fb1e2dc31936
Steven Janzou <steven@janzouconsulting.com>
2026-08-09 23:50:37 -0600

    Update path for ortools for Windows runner

--- prepare_build.py
+++ prepare_build.py
@@ -225,6 +225,7 @@ def do_stage():
 #    stage_ortools(pkg_dir, staged)
     if sys.platform == "win32":
         # copy over dlls
+        ortoolsdir = os.environ.get("ORTOOLSDIR")
         dll_subdir = "bin"
         patterns = ["*dll"]
         ortools_dll_dir = Path(ortoolsdir) / dll_subdir
@@ -237,7 +238,7 @@ def do_stage():
                 shutil.copy2(str(dll_path), str(dest))
                 staged.append(str(dest))
                 print(f"  Staged OR-Tools DLL: {dest.name}")
-    
+
 
     print("Generating defaults...")
     stage_defaults(pkg_dir, staged)
